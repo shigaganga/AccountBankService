848365ddb59914131111e84bfc0e43bc930e7ff2
Shiga Gangadharan <shiga@tekarch.com>
2024-12-17 11:33:53 -0500

    AccountBankService original

--- src/main/java/com/tekarch/AccountBankService/Repositories/AccountRepository.java
+++ src/main/java/com/tekarch/AccountBankService/Repositories/AccountRepository.java
@@ -1,8 +1,18 @@
 package com.tekarch.AccountBankService.Repositories;
+import com.tekarch.AccountBankService.Models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+public interface AccountRepository extends JpaRepository <Account,Long>{
+    // List<Account> findByUserId(Long userId);
+    //Optional<Account> findByAccountId(Long accountId);
+    // Optional<Account> findByUserIdAndAccountId(Long userId, Long accountId);
+    // Find all accounts by userId
+    List<Account> findByUserId(Long userId);
 
+    // Find an account by accountId
+    Account findByAccountId(Long accountId);
 
-
-public interface AccountRepository {
-
+    // Find an account by both userId and accountId
+    Account findByUserIdAndAccountId(Long userId, Long accountId);
 }
 
--- src/main/java/com/tekarch/AccountBankService/Services/Interface/AccountService.java
+++ src/main/java/com/tekarch/AccountBankService/Services/Interface/AccountService.java
@@ -6,5 +6,13 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface AccountService {
-
+    Account createAccount(Account account);
+    List<Account> getAllAccounts();
+    List<Account> getAccountsByUserId(Long userId);
+    Account getAccountByAccountId(Long accountId);
+    Account updateAccount(Long accountId, Account updatedAccount);
+    Account updateAccountByUserIdAndAccountId(Long userId, Long accountId, Account updatedAccount);
+    boolean deleteAccount(Long accountId);
+    List<Account> getBalancesByUserId(Long userId);
+    BigDecimal getBalanceByAccountId(Long accountId);
 }
